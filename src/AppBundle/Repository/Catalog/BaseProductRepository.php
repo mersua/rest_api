4d4b16c0aca2acd3d7ffc8fffe190f530c484323
mersua <ygrobovenko@gmail.com>
2018-10-19 15:57:04 +0300

    fix

--- src/AppBundle/Repository/Catalog/BaseProductRepository.php
+++ src/AppBundle/Repository/Catalog/BaseProductRepository.php
@@ -3,15 +3,7 @@
 namespace AppBundle\Repository\Catalog;
 
 use AppBundle\Entity\Catalog\BaseProduct;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityRepository;
 
 class BaseProductRepository
 {
-    private $entityRepository;
-    private $entityManager;
-    public function __construct(EntityRepository $entityRepository, EntityManagerInterface $entityManager) {
-        $this->entityRepository = $entityRepository;
-        $this->entityManager = $entityManager;
-    }
 }
